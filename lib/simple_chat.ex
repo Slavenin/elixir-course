23cb5084a311abc13c61fc1fc542a7519c75e0a6
Maxim Barulin <slavenin999@yandex.ru>
2020-10-05 12:33:18 +0300

    hello

--- lib/simple_chat.ex
+++ lib/simple_chat.ex
@@ -1,3 +1,32 @@
 defmodule SimpleChat do
+  @moduledoc """
+  Documentation for `SimpleChat`.
+  """
 
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SimpleChat.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def hello(name) when is_binary(name) do
+    "hello #{name}"
+  end
+
+  def hello(%{"name" => name, "age" => age}) do
+    hello("#{name} #{age}")
+  end
+
+  def hello(users) when is_list(users) do
+    Enum.each(users, fn user -> hello(user) end)
+  end
+
+  def hello(_), do: :error
 end
