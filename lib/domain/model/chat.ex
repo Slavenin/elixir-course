73c03cfbdddd37caed981a8bc1caad53579fb352
Maxim Barulin <slavenin999@yandex.ru>
2020-10-16 16:55:45 +0300

    step 4

--- lib/domain/model/chat.ex
+++ lib/domain/model/chat.ex
@@ -1,12 +1,18 @@
 defmodule SimpleChat.Domain.Model.Chat do
   @moduledoc false
 
+  alias SimpleChat.Domain.Service.ChatUsers, as: ChatUsersService
+
+  @type t :: %__MODULE__{}
+
   defstruct name: nil, id: nil
 
-  @chat_users_servise SimpleChat.Domain.Service.ChatUsers
+  @chat_users_servise ChatUsersService
 
+  @spec new(any) :: __MODULE__.t()
   def new(name), do: %__MODULE__{name: name, id: generate_id()}
 
+  @spec get_users(__MODULE__.t()) :: []
   def get_users(%__MODULE__{id: id}), do: @chat_users_servise.get_users_in_chat(id)
 
   defp generate_id() do
--- lib/domain/model/user.ex
+++ lib/domain/model/user.ex
@@ -1,11 +1,17 @@
 defmodule SimpleChat.Domain.Model.User do
   @moduledoc false
 
+  alias SimpleChat.Domain.Service.ChatUsers, as: ChatUsersService
+
+  @type t :: %__MODULE__{}
+
   defstruct login: nil
 
-  @chat_users_servise SimpleChat.Domain.Service.ChatUsers
+  @chat_users_servise ChatUsersService
 
+  @spec new(String.t()) :: __MODULE__.t()
   def new(login), do: %__MODULE__{login: login}
 
+  @spec get_chats(__MODULE__.t()) :: []
   def get_chats(%__MODULE__{login: login}), do: @chat_users_servise.get_user_chats(login)
 end
