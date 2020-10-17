9e4051776176c82c4a3932cc0433ed89871b630d
Maxim Barulin <slavenin999@yandex.ru>
2020-10-17 16:41:00 +0300

    step 5

--- lib/domain/model/chat.ex
+++ lib/domain/model/chat.ex
@@ -2,6 +2,7 @@ defmodule SimpleChat.Domain.Model.Chat do
   @moduledoc false
 
   alias SimpleChat.Domain.Service.ChatUsers, as: ChatUsersService
+  alias SimpleChat.Domain.Model.User
 
   @type t :: %__MODULE__{}
 
@@ -14,6 +15,12 @@ defmodule SimpleChat.Domain.Model.Chat do
 
   @spec get_users(__MODULE__.t()) :: []
   def get_users(%__MODULE__{id: id}), do: @chat_users_servise.get_users_in_chat(id)
+
+  @spec join_chat(User.t()) :: {:ok, true} | {:error, binary()}
+  def join_chat(%User{} = user), do: @chat_users_servise.add_user_to_chat(user)
+
+  @spec leave_chat(User.t()) :: {:ok, true} | {:error, binary()}
+  def leave_chat(%User{} = user),  do: @chat_users_servise.remove_user_from_chat(user)
 
   defp generate_id() do
     datetime =
--- lib/domain/service/chat_users.ex
+++ lib/domain/service/chat_users.ex
@@ -6,4 +6,12 @@ defmodule SimpleChat.Domain.Service.ChatUsers do
   def get_users_in_chat(id) do
     []
   end
+
+  def add_user_to_chat(user) do
+    nil
+  end
+
+  def remove_user_from_chat(user) do
+    nil
+  end
 end
