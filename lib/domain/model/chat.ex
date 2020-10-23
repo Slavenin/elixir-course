ef314f9dcbf33248626990ffacd584f5ada8f7c9
Maxim Barulin <slavenin999@yandex.ru>
2020-10-23 11:55:49 +0300

    step 9

--- lib/domain/model/chat.ex
+++ lib/domain/model/chat.ex
@@ -13,14 +13,15 @@ defmodule SimpleChat.Domain.Model.Chat do
   @spec new(String.t()) :: __MODULE__.t()
   def new(name), do: %__MODULE__{name: name, id: generate_id()}
 
-  @spec get_users(__MODULE__.t()) :: []
+  @spec get_users(__MODULE__.t()) ::
+          {:error, :not_found} | {:ok, [SimpleChat.Domain.Model.User.t(), ...]}
   def get_users(%__MODULE__{id: id}), do: @chat_users_servise.get_users_in_chat(id)
 
-  @spec join_chat(User.t(), binary()) :: {:ok, true} | {:error, binary()}
+  @spec join_chat(User.t(), binary) :: :ok | {:error, :not_found}
   def join_chat(%User{} = user, chat_id), do: @chat_users_servise.add_user_to_chat(user, chat_id)
 
-  @spec leave_chat(User.t(), binary()) :: {:ok, true} | {:error, binary()}
-  def leave_chat(%User{} = user, chat_id),  do: @chat_users_servise.remove_user_from_chat(user)
+  @spec leave_chat(User.t(), binary) :: true
+  def leave_chat(%User{} = user, chat_id),  do: @chat_users_servise.remove_user_from_chat(user, chat_id)
 
   defp generate_id() do
     datetime =
