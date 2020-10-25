ead236ecc013346da0206067282b0fe3a2734038
Maxim Barulin <slavenin999@yandex.ru>
2020-10-25 15:56:43 +0300

    step 12

--- README.md
+++ README.md
@@ -18,9 +18,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/simple_chat](https://hexdocs.pm/simple_chat).
-
-pid = SimpleChat.run()
-send(pid, {:new_user, "test"})
-send(pid, {:new_chat, "test_chat"})
-send(pid, {:join_chat, "test", :ets.first(:chats)})
-send(pid, {:send_message_to_chat, "test message", :ets.first(:chats), "test"})
--- lib/simple_chat.ex
+++ lib/simple_chat.ex
@@ -13,6 +13,44 @@ defmodule SimpleChat do
     users_table()
   ]
 
+  @spec new_user(atom | pid | port | {atom, atom}, String.t()) :: atom | pid | port | {atom, atom}
+  def new_user(pid, login) do
+    send(pid, {:new_user, login})
+    pid
+  end
+
+  @spec new_chat(atom | pid | port | {atom, atom}, String.t()) :: atom | pid | port | {atom, atom}
+  def new_chat(pid, name) do
+    send(pid, {:new_chat, name})
+    pid
+  end
+
+  @spec join_chat(atom | pid | port | {atom, atom}, String.t(), String.t()) ::
+          atom | pid | port | {atom, atom}
+  def join_chat(pid, user_login, chat_id) do
+    send(pid, {:join_chat, user_login, chat_id})
+    pid
+  end
+
+  @spec send_message(atom | pid | port | {atom, atom}, String.t(), String.t(), String.t()) ::
+          atom | pid | port | {atom, atom}
+  def send_message(pid, message, chat_id, frtom_user_login) do
+    send(pid, {:send_message_to_chat, message, chat_id, frtom_user_login})
+    pid
+  end
+
+
+  def test_it do
+    pid = run()
+    new_user(pid, "test")
+    :timer.sleep(200)
+    new_chat(pid, "test_chat")
+    :timer.sleep(200)
+    join_chat(pid, "test", :ets.first(:chats))
+    :timer.sleep(200)
+    send_message(pid, "test message", :ets.first(:chats), "test")
+  end
+
   @spec run :: no_return
   def run() do
     Enum.each(@tables, &init_table/1)
