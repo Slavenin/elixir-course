8cceb293b16eea7af901652a969b17162d15c521
Maxim Barulin <slavenin999@yandex.ru>
2020-10-26 11:13:20 +0300

    step 12

--- lib/simple_chat.ex
+++ lib/simple_chat.ex
@@ -4,7 +4,7 @@ defmodule SimpleChat do
   import SimpleChat.Infrastructure.Repository.Base,
     only: [chat_users_table: 0, chats_table: 0, user_chats_table: 0, users_table: 0]
 
-  alias SimpleChat.Domain.Service.Message, as: MessageService
+  alias SimpleChat.Infrastructure.Service.Message, as: MessageService
 
   @tables [
     chat_users_table(),
